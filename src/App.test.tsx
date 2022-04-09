d94ca9aaa1de7fd72a1fae313a720572ac97e68d
anoopjadhav <anoop.jadhav@metro-external.digital>
2022-04-10 00:11:46 +0530

    Testing library added

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import {render} from "@testing-library/react-native"
+import App from "./App"
+describe("App",()=>{
+    it("should render the component",()=>{
+        const {toJSON} = render(<App/>)
+        expect(toJSON).toMatchSnapshot();
+    })
+})
